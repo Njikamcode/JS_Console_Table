a98411e6c95ba625659494ebb5a234fa0d077f8c
Njikamcode <33057880+Njikamcode@users.noreply.github.com>
2022-03-04 18:56:52 +0100

    Add files via upload

---
+++ console_table.js
@@ -0,0 +1,7 @@
+const series = [
+    {name: 'Game of Thrones', year: 2011, genre: 'Action', rating: 9.1},
+    {name: 'Breaking Bad', year: 2008, genre: 'Drama', rating: 8.5},
+    {name: 'The Big Bang Theory', year: 2007, genre: 'Comedy', rating: 8.5},
+];
+
+console.table(series);
